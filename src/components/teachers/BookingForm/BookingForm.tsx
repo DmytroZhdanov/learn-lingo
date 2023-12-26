5d71332fd8fbef9338718c6332a62daeb3431f66
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-12-26 16:51:24 +0200

    Add basic markup for BookingForm component

--- src/components/teachers/BookingForm/BookingForm.tsx
+++ src/components/teachers/BookingForm/BookingForm.tsx
@@ -1,7 +1,48 @@
+import Button from "components/common/Button";
 import { FC, ReactElement } from "react";
 
 const BookingForm: FC = (): ReactElement => {
-  return <div>BookingForm component</div>;
+  return (
+    <div>
+      <h2>Book trial lesson</h2>
+
+      <p>
+        Our experienced tutor will assess your current language level, discuss your learning goals,
+        and tailor the lesson to your specific needs.
+      </p>
+
+      <div></div>
+
+      <form>
+        <fieldset>
+          <legend>What is your main reason for learning English?</legend>
+
+          <input type="checkbox" id="business" name="business" />
+          <label htmlFor="business">Career and business</label>
+
+          <input type="checkbox" id="kids" name="kids" />
+          <label htmlFor="kids">Lesson for kids</label>
+
+          <input type="checkbox" id="abroad" name="abroad" />
+          <label htmlFor="abroad">Living abroad</label>
+
+          <input type="checkbox" id="exams" name="exams" />
+          <label htmlFor="exams">Exams and coursework</label>
+
+          <input type="checkbox" id="culture" name="culture" />
+          <label htmlFor="culture">Culture, travel or hobby</label>
+        </fieldset>
+
+        <input type="text" />
+
+        <input type="email" />
+
+        <input type="tel" />
+
+        <Button padding="16px">Book</Button>
+      </form>
+    </div>
+  );
 };
 
 export default BookingForm;
