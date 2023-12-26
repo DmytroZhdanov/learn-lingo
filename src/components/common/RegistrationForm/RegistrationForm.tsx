da3b75c8220a080b98e3e916c3726ccf4804d53c
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-12-26 16:40:05 +0200

    Add markup to RegistrationForm component

--- src/components/common/RegistrationForm/RegistrationForm.tsx
+++ src/components/common/RegistrationForm/RegistrationForm.tsx
@@ -1,7 +1,27 @@
 import { FC, ReactElement } from "react";
+import Button from "../Button";
 
 const RegistrationForm: FC = (): ReactElement => {
-  return <div>RegistrationForm component</div>;
+  return (
+    <div>
+      <h2>Registration</h2>
+
+      <p>
+        Thank you for your interest in our platform! In order to register, we need some information.
+        Please provide us with the following information
+      </p>
+
+      <form>
+        <input type="text" />
+
+        <input type="email" />
+
+        <input type="password" />
+
+        <Button padding="16px">Sign Up</Button>
+      </form>
+    </div>
+  );
 };
 
 export default RegistrationForm;
