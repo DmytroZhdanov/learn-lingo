e0de90c75d6c05d77284affb4e841efe21585975
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2024-01-03 15:40:58 +0200

    Add validation via formik and yup to LogInForm component

--- src/components/common/LogInForm/LogInForm.tsx
+++ src/components/common/LogInForm/LogInForm.tsx
@@ -1,7 +1,24 @@
 import { FC, ReactElement } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
 import Button from "../Button";
+import { LogInValidationSchema } from "./LogInValidationSchema";
+
+type TFormValues = {
+  email: string;
+  password: string;
+};
 
 const LogInForm: FC = (): ReactElement => {
+  const initialValues: TFormValues = {
+    email: "",
+    password: "",
+  };
+
+  const handleSubmit = (values: TFormValues): void => {
+    console.log(values);
+  };
+
   return (
     <div>
       <h2>Log In</h2>
@@ -11,13 +28,25 @@ const LogInForm: FC = (): ReactElement => {
         for an teacher.
       </p>
 
-      <form>
-        <input type="email" />
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={LogInValidationSchema}
+      >
+        {({ isValid, dirty }) => (
+          <Form>
+            <Field type="email" id="email" name="email" />
+            <ErrorMessage name="email" />
 
-        <input type="password" />
+            <Field type="password" id="password" name="password" />
+            <ErrorMessage name="email" />
 
-        <Button padding="16px">Log In</Button>
-      </form>
+            <Button padding="16px" type="submit" disabled={!isValid || !dirty}>
+              Log In
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
